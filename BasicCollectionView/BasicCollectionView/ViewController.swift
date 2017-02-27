ba23b52528f887822e1c2ceb5f9764d783e4c571
liushaochang <liushaochang@zgyjyx.com>
2017-02-27 09:55:52 +0800

    BasicCollectionView

---
+++ BasicCollectionView/BasicCollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BasicCollectionView
+//
+//  Created by liushaochang on 2017/2/26.
+//  Copyright © 2017年 S.L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
